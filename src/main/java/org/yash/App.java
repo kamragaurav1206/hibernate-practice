ab9c6cf735409bc0fc7410d82f887cf0d95265fd
kamra.gaurav <gauravjava1206@gmail.com>
2023-03-29 23:45:34 +0530

    first session factory created

--- src/main/java/org/yash/App.java
+++ src/main/java/org/yash/App.java
@@ -1,5 +1,9 @@
 package org.yash;
 
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        System.out.println(sessionFactory);
     }
 }
