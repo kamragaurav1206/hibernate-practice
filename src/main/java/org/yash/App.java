f1c860215ca304be3ebfae5c29c5cc170b310d1a
kamra.gaurav <gauravjava1206@gmail.com>
2023-03-30 00:09:07 +0530

    first object store in db using hibernate

--- src/main/java/org/yash/App.java
+++ src/main/java/org/yash/App.java
@@ -1,7 +1,9 @@
 package org.yash;
 
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 /**
@@ -15,5 +17,11 @@ public class App
         System.out.println( "Hello World!" );
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         System.out.println(sessionFactory);
+        Session session = sessionFactory.openSession();
+        Student st = new Student(101,"gaurav","gk@gmail.com");
+        Transaction transaction = session.beginTransaction();
+        session.save(st);
+        transaction.commit();
+        session.close();
     }
 }
